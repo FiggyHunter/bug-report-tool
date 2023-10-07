fb1eaba2ab937c6efbb9cfa2e3fa642cd1249934
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-07 18:05:07 +0200

    add user data functions

---
+++ server/dao/user.dao.js
@@ -0,0 +1,9 @@
+import User from "../models/User.model.js";
+export const getUserByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email: email });
+    return user;
+  } catch (e) {
+    return null;
+  }
+};
