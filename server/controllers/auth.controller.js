bb284a8bbcafaf99a3e82ca605128cca5e5244e0
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-07 18:04:54 +0200

    added auth controllers

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+import { getUserByEmail } from "../dao/user.dao.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { password, ...data } = req.body;
+
+  if (await getUserByEmail(data.email))
+    return res.status(400).json({ error: "User already exists!" });
+
+  try {
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
+
+    const user = new User({ ...data, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: "User created successfully!" });
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res.status(500).json({ error: "Server Exception: User was not created" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await getUserByEmail(email);
+    if (user) {
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, {
+          expiresIn: process.env.TOKEN_EXPIRATION_DURATION.toString(),
+        });
+        res.status(200).json({ message: "Logged in!", token });
+        return;
+      }
+    }
+    res.status(401).json({ error: "Invalid Credentials" });
+  } catch (error) {
+    res.status(500).json({ error: "Server Exception: Login failed" });
+  }
+};
