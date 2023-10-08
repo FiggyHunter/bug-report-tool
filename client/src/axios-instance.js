00f7bcac8f66ab7807418eadf630ad6d61213f34
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-08 18:35:32 +0200

    Add axios instance

---
+++ client/src/axios-instance.js
@@ -0,0 +1,8 @@
+import Axios from "axios";
+
+const instance = Axios.create({ baseURL: "http://localhost:4000" });
+
+instance.defaults.headers.common["x-access-token"] =
+  localStorage.getItem("token") || null;
+
+export default instance;
