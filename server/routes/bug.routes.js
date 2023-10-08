c2b0dae53b5bfdc94335036787cc0408da91fe83
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-08 10:48:21 +0200

    Frontend now gets bugs by ID

--- server/routes/bug.routes.js
+++ server/routes/bug.routes.js
@@ -1,15 +1,20 @@
 import express from "express";
 import {
+  changeCompletedStatus,
   createNewBug,
   deleteBug,
   getAllBugs,
+  getAllBugsByUser,
 } from "../controllers/bug.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router
-  .get("/", getAllBugs)
-  .post("/create", createNewBug)
-  .delete("/delete/:id", deleteBug);
+  .get("/", authMiddleware, getAllBugs)
+  .post("/create", authMiddleware, createNewBug)
+  .delete("/delete/:id", authMiddleware, deleteBug)
+  .get("/:userId", authMiddleware, getAllBugsByUser)
+  .patch("/:bugId", authMiddleware, changeCompletedStatus);
 
 export default router;
