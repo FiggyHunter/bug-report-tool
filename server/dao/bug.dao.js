c3508171bca3e623d2af9e6f44e48f99908ed77e
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-08 18:32:44 +0200

    updated bug dao

--- server/dao/bug.dao.js
+++ server/dao/bug.dao.js
@@ -2,7 +2,7 @@ import Bug from "../models/BugTicket.model.js";
 import User from "../models/User.model.js";
 import { ROLES } from "../constants.js";
 
-export const getBugsById = async (role) => {
+export const getBugsById = async (userId, role) => {
   let bugs = [];
   if (role === ROLES.QA) {
     bugs = await Bug.find({ reportedBy: userId });
