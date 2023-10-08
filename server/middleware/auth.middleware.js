23a36e063104c61faa0b627053e7ff800bf1e1b1
FiggyHunter <leonardo.roi3@gmail.com>
2023-10-08 10:47:50 +0200

    implemented auth middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const verifyJwtToken = (req, res, next) => {
+  const token = req.header("x-access-token");
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+
+    if (decoded) {
+      req.user = {
+        id: decoded.id,
+        role: decoded.role,
+        email: decoded.email,
+      };
+
+      next();
+    }
+  } catch (e) {
+    return res.status(401).send("Unauthorized!");
+  }
+};
+
+export default verifyJwtToken;
