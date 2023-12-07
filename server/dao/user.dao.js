8aaf45a5715a481aeec9d0bb740a7cd439dedc02
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-07 23:10:36 +0100

    deleted unused code

--- server/dao/user.dao.js
+++
@@ -1,9 +0,0 @@
-import User from "../models/User.model.js";
-export const getUserByEmail = async (email) => {
-  try {
-    const user = await User.findOne({ email: email });
-    return user;
-  } catch (e) {
-    return null;
-  }
-};
