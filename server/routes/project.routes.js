03158f2ce044121ada4f6d682f4d088435ab6bc2
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-10 21:01:34 +0100

    added a new route for the project rendering by id

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   getAllProjectsByUser,
   createNewProject,
+  getProjectByRequestedId,
 } from "../controllers/project.controller.js";
 // import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getAllProjectsByUser).post("/create", createNewProject);
+router
+  .get("/", getAllProjectsByUser)
+  .get("/:id", getProjectByRequestedId)
+  .post("/create", createNewProject);
 
 export default router;
