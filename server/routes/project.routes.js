7b02f982807582c800ecaed327249f9f9b6ec903
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-12 23:46:29 +0100

    added a new endpoint for the dashboard projects

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -3,6 +3,7 @@ import {
   getAllProjectsByUser,
   createNewProject,
   getProjectByRequestedId,
+  getAllRecentProjectsByUser,
 } from "../controllers/project.controller.js";
 // import authMiddleware from "../middleware/auth.middleware.js";
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router
   .get("/", getAllProjectsByUser)
+  .get("/recent", getAllRecentProjectsByUser)
   .get("/:id", getProjectByRequestedId)
   .post("/create", createNewProject);
 
