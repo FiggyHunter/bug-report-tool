3803ad41b8430182746dff4ff647b0c9a728ad0f
FiggyHunter <leonardo.roi3@gmail.com>
2023-12-14 13:56:14 +0100

    worked on bug controller

--- server/controllers/bug.controller.js
+++ server/controllers/bug.controller.js
@@ -2,6 +2,10 @@ import { getBugsById } from "../services/bug.dao.js";
 import Bug from "../models/BugTicket.model.js";
 
 export const findBugById = async (id) => {
+  /*  
+#swagger.tags = ['Bugs']
+#swagger.description = 'Finds a bug by it's ID.'   
+*/
   try {
     const bug = await Bug.findOne({ _id: id });
     return bug || null;
@@ -10,11 +14,35 @@ export const findBugById = async (id) => {
   }
 };
 
-//TODO
 export const getAllBugs = async (req, res) => {
+  const id = req.headers["project-id"];
   try {
-    const bugs = await Bug.find({});
-    res.status(200).send(bugs);
+    const bugs = await Bug.find({ projectId: id });
+
+    if (bugs.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No bugs found for the given project ID" });
+    }
+
+    res.status(200).send(bugs.reverse());
+  } catch (error) {
+    console.error("Error fetching bugs:", error);
+    res
+      .status(500)
+      .json({ error: "Server Exception: Could not retrieve bugs" });
+  }
+};
+
+export const getAllRecentBugs = async (req, res) => {
+  /*  #swagger.tags = ['Bugs']
+#swagger.description = 'Endpoint to get the recent 5 bugs from the user.' 
+  */
+  const id = req.headers["x-user-id"];
+  try {
+    const bugs = await Bug.find({ createdBy: id });
+    console.log(id);
+    res.status(200).send(bugs.reverse().slice(0, 5));
   } catch (error) {
     res
       .status(500)
@@ -23,9 +51,14 @@ export const getAllBugs = async (req, res) => {
 };
 
 export const createNewBug = async (req, res) => {
+  /*  
+#swagger.tags = ['Bugs']
+#swagger.description = 'Endpoint to get create a new bug.' 
+*/
   const data = req.body;
+  const userId = req.headers["x-user-id"];
   try {
-    await Bug.create({ ...data });
+    await Bug.create({ ...data, createdBy: userId });
     res.status(201).json({ message: "Bug created successfully!" });
   } catch (error) {
     res.status(500).json({
@@ -35,6 +68,10 @@ export const createNewBug = async (req, res) => {
 };
 
 export const deleteBug = async (req, res) => {
+  /*  
+#swagger.tags = ['Bugs']
+#swagger.description = 'Endpoint to get delete a bug.' 
+*/
   const receivedId = req.params.id;
   try {
     const bug = await findBugById(receivedId);
@@ -51,6 +88,10 @@ export const deleteBug = async (req, res) => {
 };
 
 export const getAllBugsByUser = async (req, res) => {
+  /*  
+#swagger.tags = ['Bugs']
+#swagger.description = 'Endpoint to get all bugs by userId.' 
+*/
   const { role, id } = req.user;
   try {
     const bugs = await getBugsById(id, role);
@@ -63,12 +104,21 @@ export const getAllBugsByUser = async (req, res) => {
 };
 
 export const changeCompletedStatus = async (req, res) => {
+  /*  
+#swagger.tags = ['Bugs']
+#swagger.description = 'Endpoint to change the completed status of a bug.' 
+*/
   const { bugId } = req.params;
-  const { completed } = req.body;
   try {
-    const bug = await Bug.findByIdAndUpdate(bugId, { completed });
-    res.status(400).json("Updated bug successfully");
-  } catch (e) {
-    res.json("Could not update bug");
+    const bug = await Bug.findById(bugId);
+    if (!bug) {
+      return res.status(404).json({ message: "Bug not found" });
+    }
+    bug.completed = !bug.completed;
+    await bug.save();
+    res.status(200).json({ message: "Updated bug successfully", bug });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Could not update bug" });
   }
 };
